31ee2b45c18713387617e55b9521f05d03863839
Skilles <bilal@bilalmadi.com>
2022-11-15 11:05:05 -0500

    Fix for .env

--- src/server.js
+++ src/server.js
@@ -1,17 +1,8 @@
-import * as dotenv from 'dotenv';
 import express, { json } from 'express';
 
 import recipeRoutes from './routes/recipe.js';
 import authRoutes from './routes/auth.js';
 import connect from './mongo.js';
-
-let config = dotenv.config();
-
-if (config.error) {
-  throw config.error;
-} else {
-  config = config.parsed;
-}
 
 const app = express();
 
@@ -19,9 +10,9 @@ app.use(json());
 app.use(recipeRoutes);
 app.use(authRoutes);
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   // perform a database connection when server starts
-  connect(config.ATLAS_URI, (err) => {
+  connect(process.env.ATLAS_URI, (err) => {
     if (err) console.error(err);
   }).then(() => {
     console.log('Server started on port 5000');
